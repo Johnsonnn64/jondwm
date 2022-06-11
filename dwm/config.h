51ecd0b5b851bb8a9c742455bd1c32e398824ffb
Johnsonnn64 <naktacamel2@gmail.com>
2022-06-11 12:28:36 +0800

    cleanup

--- dwm/config.h
+++ dwm/config.h
@@ -1,41 +1,41 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
-static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 15;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 15;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int user_bh            = 30;       /* user assigned bar height */
-static const int vertpad                 = 15;        /* statusbar verical padding */
-static const int sidepad                 = 15;       /* statusbar side padding */
-static const unsigned int colorfultag    = 1;   /* 0 means use SchemeSel for selected tag */
-static const char *fonts[]               = { "JetBrainsMono Nerd Font:style=Medium:size=9:antialias=true",
-                                             "Material Design Icons-Regular:size=10:antialias=true", };
+static const unsigned int borderpx     = 0;    /* border pixel of windows */
+static const unsigned int snap         = 32;   /* snap pixel */
+static const int showbar               = 1;    /* 0 means no bar */
+static const int topbar                = 1;    /* 0 means bottom bar */
+static const int swallowfloating       = 0;    /* 1 means swallow floating windows by default */
+static const unsigned int gappih       = 15;   /* horiz inner gap between windows */
+static const unsigned int gappiv       = 15;   /* vert inner gap between windows */
+static const unsigned int gappoh       = 15;   /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov       = 15;   /* vert outer gap between windows and screen edge */
+static const int smartgaps             = 0;    /* 1 means no outer gap when there is only one window */
+static const int user_bh               = 30;   /* user assigned bar height */
+static const int vertpad               = 15;   /* statusbar verical padding */
+static const int sidepad               = 15;   /* statusbar side padding */
+static const unsigned int colorfultag  = 1;    /* 0 means use SchemeSel for selected tag */
+static const char *fonts[]             = { "JetBrainsMono Nerd Font:style=Medium:size=10:antialias=true",
+                                           "Material Design Icons-Regular:size=10:antialias=true", };
 
 #include "catppuccin.h"
 
-static const char *colors[][3]      = {
-  /*                     fg       bg      border */
-  [SchemeNorm]       = { gray3, black, gray2 },
-  [SchemeSel]        = { gray4, blue,  blue },
-  [SchemeTag]        = { gray3, black, black },
-  [SchemeTag1]       = { yellow,  black, black },
-  [SchemeTag2]       = { red,   black, black },
-  [SchemeTag3]       = { orange, black,black },
-  [SchemeTag4]       = { green, black, black },
-  [SchemeTag5]       = { pink,  black, black },
-  [SchemeTag6]       = { blue,   black, black },
-  [SchemeTag7]       = { green, black,black },
-  /*[SchemeTag8]       = { yellow, black, black },
-  [SchemeTag9]       = { red,  black, black },
-  [SchemeTag10]      = { pink,  black, black }, */
-  [SchemeLayout]     = { yellow, black, black },
+static const char *colors[][3] = {
+  /*                  fg      bg     border */
+  [SchemeNorm]    = { gray3,  black, gray2 },
+  [SchemeSel]     = { gray4,  blue,  blue },
+  [SchemeTag]     = { gray3,  black, black },
+  [SchemeTag1]    = { yellow, black, black },
+  [SchemeTag2]    = { red,    black, black },
+  [SchemeTag3]    = { orange, black, black },
+  [SchemeTag4]    = { green,  black, black },
+  [SchemeTag5]    = { pink,   black, black },
+  [SchemeTag6]    = { blue,   black, black },
+  [SchemeTag7]    = { green,  black, black },
+  /*[SchemeTag8]  = { yellow, black, black },
+  [SchemeTag9]    = { red,    black, black },
+  [SchemeTag10]   = { pink,   black, black }, */
+  [SchemeLayout]  = { yellow, black, black },
 };
 
 static const int tagschemes[] = { 
@@ -56,27 +56,27 @@ static const Rule rules[] = {
   /*xprop(1):
     WM_CLASS(STRING) = instance, class
     WM_NAME(STRING) = title*/
-  /* class            instance    title   tags mask  iscentered  isfloating  isterminal  noswallow  mon  spnum*/
-  { "St",             NULL,        NULL,  0,         0,          0,          1,          0,         -1,   -1 }, 
-  { "zoom",           NULL,        NULL,  1 << 6,    1,          1,          0,          0,         -1,   -1 },
-  { "Dragon-drop",    NULL,        NULL,  ~0,        0,          1,          0,          1,         -1,   -1 },
-  { NULL,             "fcen",      NULL,  0,         1,          1,          0,          1,         -1,   -1 },
-  { NULL,             "fl",        NULL,  0,         0,          1,          0,          1,         -1,   -1 },
+  /* class            instance    title  tags mask  iscentered  isfloating  isterminal  noswal  mon  spnum*/
+  { "St",             NULL,       NULL,  0,         0,          0,          1,          0,      -1,  -1 },
+  { "zoom",           NULL,       NULL,  1 << 6,    1,          1,          0,          0,      -1,  -1 },
+  { "Dragon-drop",    NULL,       NULL,  ~0,        1,          1,          0,          1,      -1,  -1 },
+  { NULL,             "fcen",     NULL,  0,         1,          1,          0,          1,      -1,  -1 },
+  { NULL,             "fl",       NULL,  0,         0,          1,          0,          1,      -1,  -1 },
   /* scratchpads*/
-  { NULL,		          "spterm",	 	 NULL,	0,		     0,          1,			     1,          0,         -1,   0  },
-  { NULL,		          "spfm",	   	 NULL,	0,		     0,          1,			     1,          0,         -1,   1  },
-  { NULL,		          "spcalcu",   NULL,	0,		     0,          1,			     0,          0,         -1,   2  },
-  { NULL,		          "spvolume",  NULL,	0,		     1,          1,			     0,          0,         -1,   3  },
-  { NULL,		          "sptop",     NULL,	0,		     1,          1,			     0,          0,         -1,   4  },
-  { NULL,		          "spcurse",   NULL,	0,		     0,          1,			     0,          0,         -1,   5  },
-  { "discord",        NULL,        NULL,  0,         0,          1,          0,          0,         -1,   6  },
-  { "YouTube Music",  NULL,        NULL,  0,         0,          1,          0,          0,         -1,   7  },
+  { NULL,		          "spterm",	 	NULL,	 0,		      1,          1,			    1,          0,      -1,  0  },
+  { NULL,		          "spfm",	   	NULL,	 0,		      1,          1,			    1,          0,      -1,  1  },
+  { NULL,		          "spcalcu",  NULL,	 0,		      1,          1,			    0,          0,      -1,  2  },
+  { NULL,		          "spvol",    NULL,	 0,		      1,          1,			    0,          0,      -1,  3  },
+  { NULL,		          "sptop",    NULL,	 0,		      1,          1,			    0,          0,      -1,  4  },
+  { NULL,		          "spcurse",  NULL,	 0,		      1,          1,			    0,          0,      -1,  5  },
+  { "discord",        NULL,       NULL,  0,         1,          1,          0,          0,      -1,  6  },
+  { "YouTube Music",  NULL,       NULL,  0,         1,          1,          0,          0,      -1,  7  },
 };
 
 /* layout(s) */
-static const float mfact     = 0.50; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const float mfact = 0.50; /* factor of master area size [0.05..0.95] */
+static const int nmaster = 1;    /* number of clients in master area */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -113,7 +113,7 @@ typedef struct {
 const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
 const char *spcmd2[] = {"st", "-n", "spfm", "-g", "110x33", "-e", "lfrun", NULL };
 const char *spcmd3[] = {"st", "-n", "spcalcu", "-g", "80x30", "-e", "bc", "-lq", NULL };
-const char *spcmd4[] = {"st", "-n", "spvolume", "-g", "115x20", "-e", "pulsemixer", NULL };
+const char *spcmd4[] = {"st", "-n", "spvol", "-g", "115x20", "-e", "pulsemixer", NULL };
 const char *spcmd5[] = {"st", "-n", "sptop", "-g", "115x35", "-e", "btop", NULL };
 const char *spcmd6[] = {"st", "-n", "spcurse", "-g", "100x30+1200+5", "-e", "calcurse", NULL };
 const char *dcmd[] = {"discord", NULL };
@@ -123,7 +123,7 @@ static Sp scratchpads[] = {
   {"spterm",       spcmd1},
   {"splf",         spcmd2},
   {"spcalcu",      spcmd3},
-  {"spvolume",     spcmd4},
+  {"spvol",        spcmd4},
   {"sptop",        spcmd5},
   {"spcurse",      spcmd6},
   {"dcmd",         dcmd},
@@ -200,8 +200,8 @@ static Key keys[] = {
   { 0,                            XK_Print,  spawn,          {.v = sscmd } },
 
   // test
-  {MODKEY, 						XK_w, 	   hideotherwins,  {0}},
-  {MODKEY|ShiftMask, 				XK_w, 	   restoreotherwins, {0}},
+  /* {MODKEY, 						XK_w, 	   hideotherwins,  {0}},
+  {MODKEY|ShiftMask, 				XK_w, 	   restoreotherwins, {0}}, */
   
 };
 
