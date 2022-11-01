30022707b42718c092fe2f8ca54bcf50a4686a78
Johnsonnn64 <naktacamel2@gmail.com>
2022-11-01 19:48:14 +0800

    selmon->sel->mon-> is redundant change to selmon->

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -1554,8 +1554,8 @@ movemouse(const Arg *arg)
 void
 movecenter(const Arg *arg)
 {
-	selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
-	selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
+	selmon->sel->x = selmon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
+	selmon->sel->y = selmon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
 	arrange(selmon);
 }
 
